e341a773e51118367aedb78c1a464678e70a16e6
YitingLiu97 <54900403+YitingLiu97@users.noreply.github.com>
2020-04-27 19:14:00 -0400

    add filter selection, visuals, and highlights

--- sketches/aboutIntro.js
+++ sketches/aboutIntro.js
@@ -1,5 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-funpart();
+    funpart();
+    // Add active class to the current control button (highlight it)
+
+
+    filterSelection('all');
+    var btnContainer = document.getElementById("myBtnContainer");
+    var btns = btnContainer.getElementsByClassName("btn");
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function () {
+            var current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace("active", "");
+            this.className += " active";
+        });
+
+        }
+
+    // document.getElementById('workNav').addEventListener('onclick',workNav);
 });
 
 
@@ -53,17 +69,77 @@ function random(min, max) {
 function randomLocation() {
     const w = screen.width;
     const h = screen.height;
-    console.log('w',w);
+    console.log('w', w);
     // random in the padding area of the body
     const randomX = random(0, w);
     const randomY = random(0.1 * h, 0.9 * h);
     // const randomX = random(0.1 * w, 0.9 * w);
     // const randomY = random(0.1 * h, 0.9 * h);
-    console.log(randomX,randomY);
+    console.log(randomX, randomY);
     return [randomX, randomY];
 }
 
 
 // creating falling fruits - creating bouncing motions in web - normal circles - gravity 
 
+
+// function workNav(){
+//    let workNav= document.getElementById('workNav');
+//    let list = workNav.createElement('ol');
+// //    let pcomp = list.createElement('li');
+// //    pcomp.innerHTML="pcomp";
+//    let listItems=[];
+//     let listSize = 5; 
+//    for (let i=0; i<listSize;i++){
+//        listItems[i]=list.createElement('li');
+//        list.appendChild(listItems[i]);
+//    }
+
+// // //    let listItems[]=
+// //    workNav.appendChild(list);
+// //    list.appendChild
+// //     let pcomp = listItem
+
+// // console.log()
+
+// }
+
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("filterDiv");
+    if (c == "all") c = "";
+    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        console.log('x[i]',x[i])
+        if (x[i].className.indexOf(c) > -1)
+            w3AddClass(x[i], "show");
+    }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+            console.log('arr2', arr2);
+        }
+    }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
 
